62c51c4229ebf6aec94fba7f86b35d300687f40c
fothgarrick <fothgarrick@gmail.com>
2025-07-18 20:51:44 -0700

    created card database

---
+++ mtgcreatures/data.js
@@ -0,0 +1,73 @@
+// data.js
+export const cards = [
+  {
+    name: "Shivan Dragon",
+    color: "Red",
+    type: "Dragon",
+    cost: 6,
+    image: "https://cards.scryfall.io/normal/front/1/f/1fcff1e0-2745-448d-a27b-e31719e222e9.jpg?1730489362"
+  },
+  {
+    name: "Serra Angel",
+    color: "White",
+    type: "Angel",
+    cost: 5,
+    image: "https://cards.scryfall.io/normal/front/3/c/3cee9303-9d65-45a2-93d4-ef4aba59141b.jpg?1730489152"
+  },
+  {
+    name: "Elvish Mystic",
+    color: "Green",
+    type: "Elf",
+    cost: 1,
+    image: "https://cards.scryfall.io/large/front/7/5/75918859-c93f-41b4-9ad0-a4a96c389f0d.jpg?1689998466"
+  },
+  {
+    name: "Air Elemental",
+    color: "Blue",
+    type: "Elemental",
+    cost: 5,
+    image: "https://cards.scryfall.io/normal/front/a/2/a27efec0-40c4-48bc-a21a-3af28a6529b5.jpg?1592516263"
+  },
+  {
+    name: "Post, Son of Rich",
+    color: "Black",
+    type: "Horror",
+    cost: 7,
+    image: "https://cards.scryfall.io/normal/front/a/9/a995fd27-b4c9-4620-b759-4755101e1534.jpg?1666055060"
+  },
+  {
+    name: "Avacyn, Angel of Hope",
+    color: "White",
+    type: "Angel",
+    cost: 8,
+    image: "https://cards.scryfall.io/large/front/3/1/317f1133-7cf8-4b7a-919e-88c45f8c2c3a.jpg?1689995555"
+  },
+  {
+    name: "Llanowar Elves",
+    color: "Green",
+    type: "Elf",
+    cost: 1,
+    image: "https://cards.scryfall.io/normal/front/6/a/6a0b230b-d391-4998-a3f7-7b158a0ec2cd.jpg?1731652605"
+  },
+  {
+    name: "Ghoulcaller Gisa",
+    color: "Black",
+    type: "Human",
+    cost: 5,
+    image: "https://cards.scryfall.io/normal/front/4/4/44d7906e-8a89-4644-acb5-46fbe97ab39f.jpg?1689997183"
+  },
+  {
+    name: "Stormbreath Dragon",
+    color: "Red",
+    type: "Dragon",
+    cost: 5,
+    image: "https://cards.scryfall.io/large/front/3/6/3637d9b8-87bb-478d-bfb1-59ddab7b5e4c.jpg?1743206812"
+  },
+  {
+    name: "Hypnotic Specter",
+    color: "Black",
+    type: "Specter",
+    cost: 3,
+    image: "https://cards.scryfall.io/large/front/9/6/9695f390-f791-4d64-90b2-731d6d37df1c.jpg?1561989443"
+  }
+];
